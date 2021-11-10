4126a1e54398c2c3b26ff5bee6e8217d0f54430d
wen.zhilong <wen.zhilong@vipshop.com>
2021-11-10 09:21:13 +0800

    top

--- src/main/java/com/at/Main2.java
+++ src/main/java/com/at/Main2.java
@@ -2,13 +2,11 @@ package com.at;
 
 
 import com.at.bean.ListNode;
+import com.at.test.HeapSort;
 import javafx.scene.input.Mnemonic;
 
 import javax.xml.stream.FactoryConfigurationError;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @author zero
@@ -18,6 +16,43 @@ public class Main2 {
 
     public static void main(String[] args) {
 
+
+        new Main2().addTwoNumbers(new ListNode(9,new ListNode(9)),new ListNode(9,new ListNode(9,new ListNode(9,new ListNode(9)))));
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        int num = 0,t=0;
+        ListNode head = l1;
+
+        while (l1 != null || l2 != null){
+
+            if(l1 == null){
+                t = num + l2.val;
+                l1 = new ListNode(t%10);
+                num = t /10;
+                l2 = l2.next;
+            }else if(l2 ==null){
+                t = num + l1.val;
+                l1.val = t%10;
+                num = t /10;
+            }else {
+                t = num + l1.val + l2.val;
+                l1.val = t%10;
+                num = t/10;
+                l2 = l2.next;
+
+            }
+
+            l1 = l1.next;
+
+        }
+
+        if(num != 0) l1.next = new ListNode(num);
+
+        return head;
 
     }
 
