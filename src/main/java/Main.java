cde483fc1244dbd1f0c2c77e711f617da72af266
wen.zhilong <wen.zhilong@vipshop.com>
2022-02-09 21:24:18 +0800

    00

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,5 +8,40 @@ import java.util.stream.Collectors;
 
 class Main {
 
+
+    Map<Integer,Boolean> map = new HashMap();
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        if(s == null || s.length() == 0 || wordDict == null || wordDict.size() == 0) return true;
+
+        return dfs(s,wordDict,0);
+
+
+    }
+
+    public boolean dfs(String s, List<String> wordDict,int index){
+
+        if(index >= s.length()) return true;
+        if(map.containsKey(index)) return map.get(index);
+
+
+        for(int i = 0;i < wordDict.size();i++){
+            String w = wordDict.get(i);
+            if(index+w.length() > s.length()) continue;
+            String ss = s.subtring(index,index+w.length());
+
+            if(w.equals(ss)){
+                map.put(index,dfs(s,wordDict,index+w.length()));
+                if(map.get(index)) return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+
 }
 
