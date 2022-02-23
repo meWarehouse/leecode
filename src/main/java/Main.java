9f03ac076434edfb5e3e9afb267098f2eb8223a3
wen.zhilong <wen.zhilong@vipshop.com>
2022-02-23 08:57:28 +0800

    575. 分糖果

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,51 @@ import java.util.stream.Collectors;
 
 class Main {
 
+    public static void main(String[] args) {
+
+        final Main main = new Main();
+
+        main.restoreIpAddresses("0000");
+
+    }
+
+    List<String> res = new ArrayList();
+
+    public List<String> restoreIpAddresses(String s) {
+
+        if(s == null || s.length() == 0) return res;
+
+        dfs(s,0,"",0);
+
+        return res;
+    }
+
+    public void dfs(String s,int index,String ans,int dot){
+
+      if(dot == 3 && s.length() - index > 3) return;
+
+      if(index >= s.length()){
+          res.add(ans);
+          return;
+      }
+
+
+
+
+
+
+    }
+
+    public boolean isValid(String s,int index ,int count){
+        if(index + count >= s.length()) return false;
+
+        if(s.charAt(index) == '0') return false;
+        String tmp = "";
+        for(int i = index;i < count + index;i++){
+            tmp+=s.charAt(i);
+        }
+        return Integer.parseInt(tmp) > 255 ? false : true;
+    }
 }
 
 
