30f5b6a1486d3c579710a463c7022e2bdd96714b
zero <zero@zero.com>
2022-03-03 00:07:24 +0800

    美团2021校招笔试-编程题(通用编程试题,第10场) 1 2 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Stack;
 
-class Main {
-
-    public static void main(String[] args) {
-        int c = '(';
-
-        System.out.println(c);
-
-        System.out.println((char) c);
-
-        new Main().coinChange(new int[]{2},3);
+public class Main {
 
 
-    }
-
-    public int coinChange(int[] coins, int amount) {
-
-        int len = coins.length;
-
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0] = 0;
-
-        for(int i = 1;i <= amount;i++){
-
-            for(int j = 0;j < len;j++){
-
-                if(coins[j] <= i){
-                    dp[i] = Math.min(dp[i],dp[i-coins[j]] + 1);
-                }
-
-            }
-
-        }
-
-        if(dp[amount] == Integer.MAX_VALUE) return -1;
-
-        return  dp[amount];
 
 
-    }
+
 
 
 }
