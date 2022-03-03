329590831d530947ab047eb3c98027aad382d125
zero <zero@zero.com>
2022-03-03 18:37:37 +0800

    美团2021校招笔试-编程题(通用编程试题,第9场) 1 2 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.Inet4Address;
-import java.util.*;
-
 public class Main {
-
-    public static void main(String[] args) {
-
-        new Main().decodeString("3[a]2[bc]");
-
-    }
 
 
 }
