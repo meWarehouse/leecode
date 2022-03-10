5b73a7aef17db87472b8315529bac92de3267e76
zero <zero@zero.com>
2022-03-10 14:30:52 +0800

    406. 根据身高重建队列

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Main {
 
 
+    public int[][] reconstructQueue(int[][] people) {
+
+        Arrays.sort(people,((o1, o2) -> {
+            if(o1[0] != o2[0]) return o2[0] - o1[0];
+            else return o1[1] - o2[1];
+        }));
+
+        List<int[]> ans = new ArrayList<>();
+
+        for (int[] person : people) {
+            ans.add(person[1],person);
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+
+
+    }
 
 }
