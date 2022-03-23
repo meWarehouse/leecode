b84156fa445bdae0689a89611e5da37f275606e6
zero <zero@zero.com>
2022-03-23 20:39:24 +0800

    73. 矩阵置零

--- problems/leecodeTOP100.md
+++ problems/leecodeTOP100.md
@@ -4660,7 +4660,57 @@ public boolean isValid(String s) {
     }
 ```
 
+### [73. 矩阵置零](https://leetcode-cn.com/problems/set-matrix-zeroes/)
+```java
 
+    public void setZeroes(int[][] matrix) {
+
+        boolean row0 = false, col0 = false;
+
+        int i = 0, j = 0;
+
+        for (i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+
+        for (j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                col0 = true;
+                break;
+            }
+        }
+
+
+        for (i = 1; i < matrix.length; i++) {
+            for (j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (i = 1; i < matrix.length; i++) {
+            for (j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+
+
+        if (row0) {
+            for (i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+        }
+
+        if (col0) {
+            for (j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
+        }
+
+    }
+
+```
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,55 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(new Main().mySqrt(2147395599));
-
 
     }
 
+    public void setZeroes(int[][] matrix) {
 
+        boolean row0 = false, col0 = false;
+
+        int i = 0, j = 0;
+
+        for (i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+
+        for (j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                col0 = true;
+                break;
+            }
+        }
+
+
+        for (i = 1; i < matrix.length; i++) {
+            for (j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (i = 1; i < matrix.length; i++) {
+            for (j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+
+
+        if (row0) {
+            for (i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+        }
+
+        if (col0) {
+            for (j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
+        }
+
+    }
 
 }
 
